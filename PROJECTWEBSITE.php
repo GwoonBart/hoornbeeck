d7b2441d6f8375c40f8344b32c0466f0591bb8f6
GwoonBart <bart@famvanderstelt.nl>
2024-10-18 15:59:44 +0200

    Update PROJECTWEBSITE.php

--- PROJECTWEBSITE.php
+++ PROJECTWEBSITE.php
@@ -1,13 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="PROJECTWEBSITE.css">
-    <title>Hoornbeeck</title>
-
+    <title>Website Titel</title>
+    <link rel="stylesheet" href="projectcode.css">
 </head>
 <body>
-    
+
+<div class="wrapper">
+    <header>
+        <h1>Hoornbeeck College</h1>
+        <img src="Hoornbeeck_beeldmerk_groot.png" alt="Logo">
+    </header>
+
+    <div class="container">
+        <div class="main-content">
+            <img src="jouw-afbeelding.jpg" alt="Foto" style="width: 100%; height: auto;"> 
+        
+        <aside class="sidebar">
+            <p>Carnissesingel<br>210<br>3084 NA<br>Rotterdam</p>
+            <p>Inloop<br>19:30</p>
+            <p>Afsluiting<br>21:15</p>
+        </aside>
+    </div>
+
+    <footer>
+        <p>Copyright © 2024 Hoornbeeck College</p>
+    </footer>
+</div>
+
 </body>
 </html>
